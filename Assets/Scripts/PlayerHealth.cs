1159d7fad3d019ed140e2d0ab81a584955b7a19d
GabrielHaddad <gabrielphaddad@yahoo.com.br>
2021-07-19 15:38:20 -0300

    implemented health bar mechanism

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    public HealthBarScript healthBarScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        healthBarScript.SetMaxHealth(maxHealth);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TakeDamage(int damage)
+    {
+        currentHealth -= damage;
+
+        healthBarScript.SetHealth(currentHealth);
+    }
+}
